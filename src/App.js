b8897f7afebf5813cad4139923180957bdf73fd0
reduction-choi <126326904+reduction-choi@users.noreply.github.com>
2025-10-07 04:21:14 +0000

    commit 1

--- src/App.js
+++ src/App.js
@@ -1,23 +1,50 @@
 import logo from './logo.svg';
 import './App.css';
-
+import { useState } from 'react';
 function App() {
+  const [text, setText] = useState(null);
+  function increment(number) {
+    const promise = new Promise((resolve, reject) => {
+      // resolve는 성공, reject는 실패
+      setTimeout(() => {
+        const result = number + 10;
+        if (result > 50) { // 50보다 높으면 에러 발생시키기
+          const e = new Error('NumberTooBig');
+          return reject(e);
+        }
+        resolve(result); // number 값에 +10 후 성공 처리
+      }, 1000)
+    });
+    return promise;
+  }
+  const onClick = async () => {
+    try { // try/catch 구문을 사용하여 에러를 처리합니다.
+    let result = await increment(0);
+    console.log(result);
+    setText(result);
+    result = await increment(result);
+    console.log(result);
+    setText(result);
+    result = await increment(result);
+    console.log(result);
+    setText(result);
+    result = await increment(result);
+    console.log(result);
+    setText(result);
+    result = await increment(result);
+    console.log(result);
+    setText(result);
+    result = await increment(result);
+    console.log(result);
+    setText(result);
+    } catch (e) {
+      console.log(e);
+    }
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <button onClick={onClick}>시작</button>
+      <h1>{text}</h1>
     </div>
   );
 }
