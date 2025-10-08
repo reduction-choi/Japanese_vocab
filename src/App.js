773f498ca1034141f722144aac377e7b43d27cf3
reduction-choi <126326904+reduction-choi@users.noreply.github.com>
2025-10-08 07:19:36 +0000

    develop branch made. Many todos left for react

--- src/App.js
+++ src/App.js
@@ -2,31 +2,29 @@ import axios from 'axios';
 import './App.css';
 import { useState } from 'react';
 function App() {
-  const [text, setText] = useState(null);
+  const [words, setWords] = useState([]);
+  const appendWords = (element) => {
+    setWords(words + element);
+    //this may not work. Look for the book to solve prob.
+  }
   const onClick = async () => {
     const token = 'acd4892e-0f94-4d49-ab83-e15049ea0f96';
     const headers = new Headers({
       Authorization: 'Bearer ' + token,
     });
     try{
-      let apidata = await axios('https://api.wanikani.com/v2/subjects',{
+      let apidata = await axios('https://api.wanikani.com/v2/subjects?types=vocabulary&levels=1',{
         method: 'GET',
         headers: headers
       });
-      setText(apidata.data);
-      // let grab = (endpoint) => {
-      // fetch('https://api.wanikani.com/v2/' + endpoint, {
-      //     method: 'GET',
-      //     headers: headers
-      //   }
-      // )
-      //   .then(response => response.json())
-      //   .then(responseBody => {
-      //     console.log(responseBody);
-      //     setText(responseBody);
-      //   });
-      // }
-      // grab("subjects");
+      console.log(apidata.data["data"]);
+      for(const wordinfo of apidata.data["data"]){
+        const object = {
+          meaning: wordinfo["data"]["meanings"][0]["meaning"],
+          hiragana: wordinfo["data"]["readings"][0]["reading"]
+        };
+        appendWords(object);
+      }
     }
     catch(e){
       console.log(e);
@@ -35,7 +33,7 @@ function App() {
   return (
     <div>
       <button onClick={onClick}>시작</button>
-      {text && <textarea rows={7} value={JSON.stringify(text, null, 2)} readOnly={true} />}
+      <Wordlist words={words}></Wordlist>
     </div>
   );
 }
---
+++ src/WordlistItem.js
@@ -0,0 +1,14 @@
+const WordlistItem = () => {
+    return (
+        <div className="WordlistItem">
+            <div>
+                {meaning}
+            </div>
+            <div>
+                {hiragana}
+            </div>
+        </div>
+    )
+}
+
+export default WordlistItem;
